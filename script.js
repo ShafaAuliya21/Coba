86d3a4eb522045b0402dbe9eca75bc2bd16d57d9
shafaauliya <shafa.auliya21@students.unila.ac.id>
2024-04-17 11:45:24 +0700

    Coba

---
+++ script.js
@@ -0,0 +1,125 @@
+// const nilai = 1;
+// const string = "Shafa Auliya";
+// console.log(nilai);
+
+// let nama = "Shafa"
+// if(nama == "Auliya"){
+//     console.log("Nama kamu benar adalah Auliya");
+// }else if(nama == "Shafa"){
+//     console.log("Nama kamu adalah Shafa");
+// }else{
+//     console.log("Kamu siapa saya ga kenal")
+// }
+
+// const nilai = 50;
+// if(nilai >= 70 && nilai <= 100){
+//     console.log("Nilai kamu sudah lulu kkm");
+// }else if(nilai >= 50 && nilai <= 69){
+//     console.log("kamu harus bajar lebih giat");
+// }else if(nilai <= 49){
+//     console.log("Kamu tidak lulus MSIB");
+// }else{
+//     console.log("Nilai kamu tidak valid");
+// }
+
+// const nama = "Abia"
+// switch (nama){
+//     case "Shafa":
+//         console.log("nama kamu adalah Shafa");
+//         break;
+//     case "Auliya":
+//         console.log("nama kamu adalah Auliya");
+//         break;
+//     default:
+//         console.log("saya tidak kenal");
+// }
+
+// const jalanan = "lampu merah";
+// const mobil = "ambulan";
+// switch(jalanan){
+//     case "lampu hijau":
+//         console.log("silahkan jalan");
+//         break;
+//     case "lampu kuning":
+//         console.log("hati hati ya sayang");
+//         break;
+//     case "lampu merah":
+//         if(mobil=="ambulan"){
+//             console.log("boleh lewat");
+//         }else{
+//             console.log("berhenti");
+//         }
+//         break;
+//     default:
+//         console.log("gada lampu jalan");
+// }
+
+// for(let i = 0; i < 100; i++){
+//     console.log(i);
+// }
+
+// for(let i = 1; i < 100; i++){
+//     if(i == 10){
+//         break;
+//     }
+//     console.log(i);
+// }
+
+// const namaKota = {
+//     A : "Aceh", 
+//     B : "Bandung", 
+//     C : "Cirebon",
+//     D : "Depok"
+// };
+
+// console.log(namaKota);
+
+// for(let key in namaKota){
+//     console.log(key, namaKota[key]);
+// }
+
+// const mentor = {
+//     nama : "Hanif",
+//     umur : 20,
+//     jenisKelamin : "laki-laki",
+//     role : "MT"
+// };
+// for(let key in mentor){
+//     console.log(mentor[key]);
+// }
+
+// const array = ["shinta", "miranda", "lian", "izzldin", "nina"];
+// for(let key of array){
+//     console.log(key);
+// }
+
+// const nama = new Set();
+// nama.add("shafa");
+// nama.add("budi");
+// for(let key of nama){
+//     console.log(key);
+// }
+
+// const nama = new Map();
+// nama.set(1, "hanif");
+// nama.set(2, "budi");
+// nama.set(3, "shafa")
+
+// for(let key of nama){
+//     for(let i of key){
+//         console.log(i);
+//     }
+// }
+
+let i = 11;
+i = 1;
+while(i < 10){
+    console.log(i);
+    i++;
+}
+
+// do{
+//     console.log("saya belajar");
+//     i++;
+// } while(i<20);
+
